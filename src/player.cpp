61c79874240e272bd9ebe7d53455eae49e03a912
Awgan <raf.szlachetka@gmail.com>
2022-10-29 13:25:35 +0200

    Implement some member function

---
+++ src/player.cpp
@@ -0,0 +1,46 @@
+
+#include "player.hpp"
+
+Player::Player()
+: m_hero{nullptr}
+, m_gold{0}
+{}
+
+Player::Player(const Hero& hero)
+: m_gold{0}
+{
+	if( m_hero == nullptr )
+	{
+		m_hero = new Hero;
+	}
+
+	*m_hero = hero;
+}
+
+void Player::add(const Hero& hero)
+{
+	if( m_hero == nullptr )
+	{
+		m_hero = new Hero;
+		*m_hero = hero;
+	}
+	else
+	{
+		std::cerr << "Ther is already the Hero\n";
+	}
+}
+void Player::change(const Hero& hero)
+{
+	if( m_hero != nullptr )
+	{
+		*m_hero = hero;
+	}
+	else
+	{
+		add(hero);
+	}
+}
+void Player::swapHero(Player& player)
+{
+	if( m_hero != nullptr &&
+}
--- src/player.hpp
+++ src/player.hpp
@@ -13,15 +13,17 @@
 class Player
 {
 	private:
-		Hero								m_hero;					//hero card
+		Hero*								m_hero;					//hero card
 		std::vector<Card> 	m_cardDeck;			//deck of cards;
-		std::vector<Card>		m_specialCard;	//deck of special card which stop the hero
-		int 								gold;						//amount of gold;
+		std::vector<Card>		m_specialCard;	//deck of special card which stop the hero; player get stop card from another players;
+		int 								m_gold;					//amount of gold;
 
 	public:
+
+		Player();
 		Player(const Hero&);
 
-		~Player(){};
+		~Player(){ delete m_hero; };
 
 		void add(const Hero&);						//add Hero card;
 		void change(const Hero&);					//cast Hero card and take another;
@@ -32,14 +34,19 @@ class Player
 		void remove(const Card&);					//remove one Card from deck
 		void swapDeck(Player&);						//swap card deck with Player;
 		int sizeDeck();										//size of card deck
-		void showdeck();									//show card in deck;
+		void showDeck();									//show card in deck;
 
 		bool isSpecial(const Card&);			//check if there is already the special card in deck
 		int sizeSpecial();								//size of special card deck
+		void showSpecial();
 		std::vector<Card>& getSpecial();	//get access to special deck to add special card
 
 		void playCard(const int n);				//play card number n from deck;
 		void choosePlace();								//choose place to play card;
+
+		const int gold() const { return m_gold; }	//get number of golds;
+		void addGold( const int gol );		//add new gold;
+		void removeGold( const int gol );		//remove from gold;
 
 		//Player class must be frind class for class Board
 
