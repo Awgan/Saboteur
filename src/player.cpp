3ac7ed72cba618896d7a5b33774f7b630376e926
Awgan <raf.szlachetka@gmail.com>
2022-10-29 12:06:56 +0200

    Add player.cpp file with first code

---
+++ src/player.cpp
@@ -0,0 +1,44 @@
+
+#define MAX_CARD_NUMBER		10		//max card in hand;
+
+#include <vector>
+
+class Card;
+class Hero;
+
+
+class Player
+{
+	private:
+		Hero								m_hero;					//hero card
+		std::vector<Card> 	m_cardDeck;			//deck of cards;
+		std::vector<Card>		m_specialCard;	//deck of special card which stop the hero
+		int 								gold;						//amount of gold;
+
+	public:
+		Player(const Hero&);
+
+		~Player(){};
+
+		void add(const Hero&);						//add Hero card;
+		void change(const Hero&);					//cast Hero card and take another;
+		void swapHero(Player&);						//swap Hero card with another Player
+
+		void initCardDeck();
+		void add(const Card&);						//add one Card to deck
+		void remove(const Card&);					//remove one Card from deck
+		void swapDeck(Player&)						//swap card deck with Player;
+		int sizeDeck();										//size of card deck
+		void showdeck();									//show card in deck;
+
+		bool isSpecial(const Card&);			//check if there is already the special card in deck
+		int sizeSpecial();								//size of special card deck
+		std::vector<Card>& getSpecial();	//get access to special deck to add special card
+
+		void playCard(const int n);				//play card number n from deck;
+		void choosePlace();								//choose place to play card;
+
+		//Player class must be frind class for class Board
+
+
+};
